4d231ffebfd5e236781c4f7c2c3e026f28d46d4a
oldlaptop <oldlaptop654@aol.com>
2012-07-17 19:48:41 -0400

    Add script to generate the effects pack from unzipped files

---
+++ generate-effects-pack.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+# Generates a zipped effects pack in content/addons by default.
+# If there is a parameter, it will be interpreted as a fully qualified
+# path for the new effects pack.
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+
+if [ $1 ]
+then
+    zip -r $1 content/addons/balance
+else
+    zip -r content/addons/balance-effects.zip content/addons/balance
+fi
